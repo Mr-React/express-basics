e89e02d539d39f8bf1fda8cf77ebe7a60eba5d1e
Mr-React <ambureomkar@gmail.com>
2023-12-14 10:44:03 +0530

    installed body parser for parsing request

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - installed express js npm package using npm install express command
 - run node index.js to start server
 - installed nodemon using npm i nodemon command
+- run npx nodemon index.js command to start server using nodemon
+- installed body-parser using npm i body-parser to parser request body
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
+  console.log(req.body);
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port} \nhttp://localhost:3000/`);
 });
-    
